2c1332a54c2d4f92233117e37dd0ca3e2fef5e9d
Diego Goulart <diegogoulart.aws@gmail.com>
2023-01-07 01:03:43 -0300

    add env values

--- src/common/axios.ts
+++ src/common/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 const token = localStorage.getItem("user") || undefined;
 
 export const clientHttp = axios.create({
-  baseURL: 'http://localhost/api/',
+  baseURL: `${import.meta.env.VITE_API_URL}/`,
   withCredentials: true,
   headers: {
     'Accept': 'application/json',
